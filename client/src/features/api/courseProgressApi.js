da0c4c60920ef75ca51935ddc35aa4a9a929c1b9
KRRISHMAHAJAN <krrish.mahajan@somaiya.edu>
2025-01-11 02:06:35 +0530

    newLinkAdd

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "https://lmsapp02.netlify.app/api/v1/progress";
+const COURSE_PROGRESS_API = "https://lms-system-sdd8.onrender.com/api/v1/progress";
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
