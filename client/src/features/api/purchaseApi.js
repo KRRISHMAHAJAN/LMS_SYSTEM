4fa63578b9503b3968c58174967c7d7c7b56e931
KRRISHMAHAJAN <krrish.mahajan@somaiya.edu>
2025-01-11 02:13:43 +0530

    Last

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "https://lmsapp02.netlify.app/api/v1/purchase";
+const COURSE_PURCHASE_API = "https://lms-system-sdd8.onrender.com/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
